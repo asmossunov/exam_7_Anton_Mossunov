1a46e75634456cde2ad5c949f886739c0d93672b
Mosunov Anton <antonmossunov@gmail.com>
2022-10-01 12:02:29 +0600

    Регистрация модели в админ-панели.
    Создан суперпользователь для подключения к админ-панели (root:root)
    Произведена регистрация модели в админ-панели

--- guest_book/book/admin.py
+++ guest_book/book/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from book.models import Record
+
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author_name', 'author_email', 'text', 'status', 'created_at', 'changed_at')
+    list_filter = ('id', 'author_name', 'author_email', 'text', 'status', 'created_at', 'changed_at')
+    search_fields = ('id', 'author_name', 'author_email', 'text', 'status', 'created_at', 'changed_at')
+    fields = ('id', 'author_name', 'author_email', 'text', 'status')
+    readonly_fields = ['id']
+
+
+admin.site.register(Record, RecordAdmin)
