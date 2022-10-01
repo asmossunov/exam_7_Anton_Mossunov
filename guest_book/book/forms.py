023b19c922396a6641c3d982d550b4f6095d4a50
Mosunov Anton <antonmossunov@gmail.com>
2022-10-01 18:40:43 +0600

    Редактирование валидации формы

--- guest_book/book/forms.py
+++ guest_book/book/forms.py
@@ -21,8 +21,3 @@ class FindForm(forms.Form):
     author_name = forms.CharField(max_length=200, required=True, label='Автор',
                                   widget=forms.TextInput({'class': 'form-input'}))
 
-    def clean_author_name(self):
-        author_name = self.cleaned_data.get('author_name')
-        if len(author_name) < 2:
-            raise ValidationError('Поле должно быть заполнено более чем одним символом')
-        return author_name
